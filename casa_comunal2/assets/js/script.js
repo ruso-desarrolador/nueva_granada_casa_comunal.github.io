26ad1af918c238d26770baef10783726471cd625
ruso-desarrolador <masterslayer2721@gmail.com>
2025-06-23 10:59:05 -0500

    Delete casa_comunal2 directory
    error 404

--- casa_comunal2/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const btnSubir = document.getElementById("btnSubir");
-window.addEventListener("scroll", () => {
-  btnSubir.style.display = window.scrollY > 300 ? "block" : "none";
-});
-
-// Scroll suave al hacer clic en el botón
-btnSubir.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
